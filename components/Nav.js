b66d3ffe141f1fbfdb64d0f712cc1664f2ce441c
khushxxl <khushaal.choithramani@gmail.com>
2021-09-12 16:17:43 +0530

    project done

---
+++ components/Nav.js
@@ -0,0 +1,26 @@
+import requests from "../utils/request";
+import { useRouter } from "next/router";
+
+const Nav = () => {
+  const router = useRouter();
+  return (
+    <nav>
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-19 overflow-x-scroll scrollbar-hide">
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h1
+            key={key}
+            onClick={() => router.push(`/?genres = ${key}`)}
+            className="last:pr-15 cursor-pointer transition duration-100 transform
+             hover:scale-125 hover:text-white
+             active:text-red-500 "
+          >
+            {title}
+          </h1>
+        ))}
+      </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ components/Results.js
@@ -0,0 +1,14 @@
+import Thumbnail from "./Thumbnail";
+
+
+function Results({ requests }) {
+  return (
+    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+      {requests.map((result) => (
+        <Thumbnail result={result} key={result.id} />
+      ))}
+    </div>
+  );
+}
+
+export default Results;
